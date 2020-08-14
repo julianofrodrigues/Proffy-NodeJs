5ed6a8a394f1ecd7dca745cb1462e055d1b1784f
Juliano Ferreira Rodrigues <julianof.rodrigues@hotmail.com>
2020-08-14 16:36:26 -0300

    iniciando desafios, mas nenhum concluido ainda

---
+++ public/scripts/removeField.js
@@ -0,0 +1,7 @@
+document.querySelector('#remove-time').addEventListener('click',removeField)
+
+function removeField() {
+    const field = document.querySelector(".schedule-item")
+    field.remove()
+  }
+  document.querySelector('#schedule-items');
